7ba654d81a88b3faa6bf0e3ff1dcdd166d8d6a0d
Mac <redsbaniasadi1381@gmail.com>
2024-08-24 05:50:43 +0000

    add insert query

--- Mongodb/mongo_query.py
+++ Mongodb/mongo_query.py
@@ -1,9 +1,13 @@
 from pymongo import MongoClient
 
 class MongoDb:
-    def __init__(self,uri="mongodb://localhost:27017/",db_name="chat_bot",collection_name="question"):
+    def __init__(self,uri="mongodb://localhost:27017/",db_name="Jarfa_Signal",collection_name="question"):
         self.client = MongoClient(uri)
         self.db = self.client[db_name]
         self.collection = self.db[collection_name]
 
+
+    def insert_question(self,question:str,vector:list):
+        data = {"question": question, "vector": vector}
+        self.collection.insert_one(data)
 
