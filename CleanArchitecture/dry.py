500c7e617d0623ae6056c96df5f4a2182cd16a5c
Mac <redsbaniasadi1381@gmail.com>
2024-09-16 09:15:00 +0000

    last change

--- CleanArchitecture/dry.py
+++ CleanArchitecture/dry.py
@@ -2,7 +2,7 @@ import csv
 
 def getdatafile(filename):
     array_data = []
-    with open(filename,'r')as f:
+    with open(filename,'r' ,newline='', encoding="utf-8")as f:
          filedata = csv.reader(f)
          for row in filedata :
              array_data.append(row)
