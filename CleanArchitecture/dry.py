67a4e5145b30e260c9f9fc26c6f996139876cb07
Mac <redsbaniasadi1381@gmail.com>
2024-10-07 09:15:00 +0000

    add function for clean data

--- CleanArchitecture/dry.py
+++ CleanArchitecture/dry.py
@@ -1,10 +1,18 @@
 import csv
-
-def getdatafile(filename):
-    array_data = []
-    with open(filename,'r' ,newline='', encoding="utf-8")as f:
-         filedata = csv.reader(f)
-         for row in filedata :
-             array_data.append(row)
-    return array_data
+import asyncio
+# def getdatafile(filename):
+#     array_data = []
+#     with open(filename,'r' ,newline='', encoding="utf-8")as f:
+#          filedata = csv.reader(f)
+#          for row in filedata :
+#              array_data.append(row)
+#     return array_data
          
+
+async def CleanGetData(text):
+        result = ''
+        for horof in text:
+            if horof != '*':
+                result += horof
+        return result
+        
