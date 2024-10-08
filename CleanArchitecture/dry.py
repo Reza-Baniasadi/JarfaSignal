e1f198329157b981e9794bf1ff577423a1359aaf
Mac <redsbaniasadi1381@gmail.com>
2024-10-08 10:14:00 +0000

    add function for use it again

--- CleanArchitecture/dry.py
+++ CleanArchitecture/dry.py
@@ -1,12 +1,14 @@
 import csv
-import asyncio
-# def getdatafile(filename):
-#     array_data = []
-#     with open(filename,'r' ,newline='', encoding="utf-8")as f:
-#          filedata = csv.reader(f)
-#          for row in filedata :
-#              array_data.append(row)
-#     return array_data
+import os
+import re
+
+def getdatafile(filename):
+    array_data = []
+    with open(filename,'r' ,newline='', encoding="utf-8")as f:
+         filedata = csv.reader(f)
+         for row in filedata :
+             array_data.append(row)
+    return array_data
          
 
 async def CleanGetData(text):
@@ -15,4 +17,35 @@ async def CleanGetData(text):
             if horof != '*':
                 result += horof
         return result
-        
+
+######## this function for get number of file ########
+      
+def getNumberFile(filevioce,all_voice_data,text):
+     if not os.listdir(filevioce):
+          with open(f'{filevioce}/0.{text}','a') as f :
+               f.write('')
+
+     for file in os.listdir(filevioce):
+          file = file.split('.')[0]
+          match = re.search(r'(/d+)$',file)  
+          if match:
+               number = match.group(1)
+          number = int(number)
+          print(number)
+          if os.path.exists(f'{filevioce}/{number}.{text}'):
+               continue
+          with open(f'{filevioce}/{number}.{text}',"w",encoding='utf-8')as f :
+               if text == 'text':
+                for onetext in all_voice_data:
+                    f.write(onetext)
+               elif text == 'ogg':
+                    for file in os.listdir(filevioce):
+                         name = file.split('.')[0]
+                         match = re.search(f'(\d+)$',name)
+                         if match :
+                              match.group(1)
+                         n = int(match)
+                         for voice in all_voice_data:
+                              with open(f'{filevioce}/{n}.{text}',"wb") as f :
+                                   f.write(voice)
+                                   n += 1
